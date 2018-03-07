e2c2ce1a2dc8b056068046e92e276d61e898fab5
Einar Ingebrigtsen <einar@dolittle.com>
2018-03-07 22:41:00 +0100

    No need for URL - adding description instead - good for menu

--- Source/BoilerPlate.js
+++ Source/BoilerPlate.js
@@ -1,17 +1,17 @@
 const _language = new WeakMap();
 const _name = new WeakMap();
-const _url = new WeakMap();
+const _description = new WeakMap();
 
 class BoilerPlate {
-    constructor(language, name, url) {
+    constructor(language, name, description) {
         _language.set(this, language);
         _name.set(this, name);
-        _url.set(this, name);
+        _description.set(this, description);
     }
 
     get name() { return _name.get(this); }
     get language() { return _language.get(this); }
-    get url() { return _url.get(this); }
+    get description() { return _desription.get(this); }
 }
 
 module.exports = BoilerPlate;
